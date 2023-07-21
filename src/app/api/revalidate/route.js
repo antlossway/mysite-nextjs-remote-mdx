49afd533e175a75fe4b5f3242b49c1cb9f49c86c
Quinn <quinn@unices.org>
2023-07-21 12:13:40 +0700

    add POST function to give github webhook to post request

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -4,23 +4,36 @@ import { NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
 
 export async function GET(req) {
-    const secret = req.nextUrl.searchParams.get('secret')
-    if (secret !== process.env.MY_SECRET_TOKEN) {
-        return new NextResponse(JSON.stringify({message: 'Invalid Token'}), {
-            status: 401,
-            statusText: 'Unauthorized',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-    }
+  const secret = req.nextUrl.searchParams.get("secret");
+  if (secret !== process.env.MY_SECRET_TOKEN) {
+    return new NextResponse(JSON.stringify({ message: "Invalid Token" }), {
+      status: 401,
+      statusText: "Unauthorized",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 
-    const path = req.nextUrl.searchParams.get('path') || '/'
+  const path = req.nextUrl.searchParams.get("path") || "/blog";
 
-    revalidatePath(path)
+  revalidatePath(path);
 
-    return NextResponse.json({
-        revalidated: true,
-        path,
-    })
+  return NextResponse.json({
+    revalidated: true,
+    path,
+  });
+}
+
+// for github webhook
+export async function POST(req) {
+  const data = await req.json(); // {path: '/blog'}
+  const path = data.path || "/blog";
+
+  revalidatePath(path);
+
+  return NextResponse.json({
+    revalidated: true,
+    path,
+  });
 }
